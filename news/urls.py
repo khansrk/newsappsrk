d36e94b2782cc7b44a9aaf268c09412edae5816e
SHAHRUKH KHAN <shahrukhkhan201994@gmail.com>
2016-09-26 05:20:39 -0700

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from newsapp import views,auth #from local module import views
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'news.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.articles_list,name='articles_list'),
+    url(r'^feeds/new', views.new_feed, name='feed_new'),
+    url(r'^feeds/', views.feeds_list,name='feeds_list'),
+    url(r'^login/', auth.login, name = 'login'),
+    url(r'^logout/', auth.logout, name = 'logout'),
+    url(r'^register/', auth.register, name = 'register'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^news/', include('newsapp.urls')),
 ]
